0f267d8fc130e98c2d30281a90440760c6b63419
Xinchro <TheXrocks@gmail.com>
2013-11-20 00:16:35 +0000

    Buildings being generated with a random height Roads being generated with random separation distance

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,12 +1,17 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
+import com.jme3.material.RenderState.FaceCullMode;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.system.AppSettings;
+import java.util.Random;
 
 /**
  * test
@@ -16,23 +21,116 @@ public class Main extends SimpleApplication {
 
     public static void main(String[] args) {
         Main app = new Main();
+	AppSettings settings = new AppSettings(true);
+	app.setSettings(settings);
+	app.setShowSettings(false);
+	app.setDisplayFps(true);
+	app.setDisplayStatView(true);
+	app.settings.setTitle("FUSCHIA");
         app.start();
     }
 
+   
     @Override
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.White);
-	this.renderer.setBackgroundColor(ColorRGBA.Blue);
+	flyCam.setMoveSpeed(50);
 	
-	mat.getAdditionalRenderState().setWireframe(true);
+	cam.setFrustumNear(1f);
+	cam.setFrustumFar(5000f);
 	
-        geom.setMaterial(mat);
+	DirectionalLight sun = new DirectionalLight();
+	sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+	sun.setColor(ColorRGBA.White);
+	rootNode.addLight(sun); 
+	
+	generateCity(25);
+    }
+    
+    public void generateCity(int citySize){
+	genRoads(citySize);
+	genBuildings(citySize);
+    }
+    
+    public void genBuildings(int citySize){
+	int xPos = 0;	
+	int yPos = 0;
+	int zPos = 0;
+	float ranHeight;
+	
+	int maxRand = 10;
+		
+	for(int i=0;i<citySize;i++)
+	{
+	    ranHeight = (float) new Random().nextInt(maxRand);
+	    while(ranHeight<=1){
+		ranHeight = (float) new Random().nextInt(maxRand);
+	    }
+	    
+	    Box b = new Box(new Vector3f(xPos, 0, zPos), .5f, ranHeight, .5f);
+	    //move box to same "ground" level
+	    b.updateGeometry(new Vector3f(b.getCenter().getX(), b.getCenter().getY()+(b.getYExtent()), b.getCenter().getZ()), b.getXExtent(), b.getYExtent(), b.getZExtent());
+	    Geometry geom = new Geometry("Box", b);
 
-        rootNode.attachChild(geom);
+	    Material mat = new Material( assetManager, "Common/MatDefs/Light/Lighting.j3md");
+	    //mat.setColor("Color", ColorRGBA.White);
+	    this.renderer.setBackgroundColor(ColorRGBA.Blue);
+
+	    mat.getAdditionalRenderState().setWireframe(false);
+	    mat.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Back);	    
+	    
+	    geom.setMaterial(mat);
+
+	    rootNode.attachChild(geom);
+	    if(xPos>=8){
+		xPos=0;
+		zPos+=2; 
+	    }else{
+		xPos+=2;
+	    }
+	}
+    }
+    
+    public void genRoads(int citySize){
+	
+	int maxRand = 10;
+	
+	int roadXPos = 0;	
+	int roadYPos = 0;
+	int roadZPos = 0;
+	float roadWidth = .5f;
+	float roadSeperation;
+	
+	boolean secondPass = false;
+	
+	for(int j=0;j<2;j++){
+	    for(int i=0;i<citySize;i++){
+		
+		roadSeperation = (float) new Random().nextInt(maxRand);
+		while(roadSeperation<=roadWidth*2){
+		    roadSeperation = (float) new Random().nextInt(maxRand);
+		}
+
+		Box plane = new Box(new Vector3f(roadXPos, 0, roadZPos), 125f, 0, roadWidth);
+		Geometry geom = new Geometry("Plane", plane);
+
+		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md"); 
+		mat.setColor("Color", ColorRGBA.Blue);
+
+		geom.setMaterial(mat);
+
+		rootNode.attachChild(geom);
+
+		if(secondPass){
+		    geom.setLocalRotation(new Quaternion(0,1,0,1));
+		    roadZPos+=roadSeperation;
+		}else{
+		    roadZPos+=roadSeperation;
+		}
+	    }
+	    roadZPos = 0;
+	    secondPass = true;
+	}
+	
     }
 
     @Override
